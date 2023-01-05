9bf20e3f0d293f7e1fff11e1b94d8bff79883e80
KerryPluto <kerry@pluto.health>
2023-01-05 14:31:43 -0500

    remove client credentials

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # Please replace these two placeholders with your own credentials
-CLIENT_ID = "3402t0v5oqplcodactc3nf53jj"
-CLIENT_SECRET = "10tuk6e2vgfaurp1gdis6eb95jh8eh7t1uonuh09rjv7m3p4ifpl"
+CLIENT_ID = "<INSERT YOUR CLIENT ID HERE>"
+CLIENT_SECRET = "<INSERT YOUR CLIENT SECRET HERE>"
 
 # Feel free to change the folder name to store logs and data
 LOGS_FILE_PATH = "clinical_trials_logs"
