d1fd1a73ac4e298fed6c845b4a7fdd54c7cac259
KerryPluto <kerry@pluto.health>
2023-01-05 12:24:25 -0500

    python code to utilize clinical trials api

---
+++ constants.py
@@ -0,0 +1,11 @@
+CLIENT_ID = "3402t0v5oqplcodactc3nf53jj"
+CLIENT_SECRET = "10tuk6e2vgfaurp1gdis6eb95jh8eh7t1uonuh09rjv7m3p4ifpl"
+
+OAUTH_ENDPOINT = "https://auth.dev.pluto-service.com/oauth2/token"
+BASE_URL = "https://pulseapi.dev.pluto-service.com/v1"
+LOGS_FILE_PATH = "/home/vagrant/go/src/aws/pluto/pulseapi-python-service/pulseapi-python-clinicaltrials"
+DATA_FILE_PATH = "/home/vagrant/go/src/aws/pluto/pulseapi-python-service/pulseapi-python-clinicaltrials"
+
+STR_CRED_INCORRECT = "Your oAuth credential is incorrect"
+STR_NO_CLINICAL_TRIALS = "No clinical trials were found"
+STR_SERVER_ERROR = "Technical issues found"
